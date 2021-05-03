504a9850504faeedd78df4cdabb4f5a9f5b26094
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-05-03 11:35:25 +0000

    [update] アソシエーション/ルーティング

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
-  has_many :active_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :passive_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followings, through: :active_relationships, source: :followed
-  has_many :followers, through: :passive_relationships, source: :follower
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :followings, through: :relationships, source: :followed
 
   attachment :profile_image, destroy: false
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  devise_for :users
+
   resources :users, only: [:show,:index,:edit,:update] do
-    member do
-      get :followings, :followers
-    end
+    resources :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
   end
-  resources :relationships, only: [:create, :destroy]
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
