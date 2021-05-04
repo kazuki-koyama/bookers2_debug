6370e8d3cadd65051c252f8b3c339c2d271b9f1e
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-05-04 02:45:31 +0000

    [add] コメント投稿失敗時のエラーメッセージ(Ajax)

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,7 +4,9 @@ class BookCommentsController < ApplicationController
     @book = Book.find(params[:book_id])
     @book_comment = current_user.book_comments.new(book_comment_params)
     @book_comment.book_id = @book.id
-    @book_comment.save
+    unless @book_comment.save
+      render 'error'
+    end
   end
 
   def destroy
