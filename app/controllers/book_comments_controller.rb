408460ad1e739dd4f8df9b13872b06a450896569
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-05-04 00:43:38 +0000

    [fix] book_commentsコントローラーの変数を、インスタンス変数化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,13 +2,14 @@ class BookCommentsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    book_comment = current_user.book_comments.new(book_comment_params)
-    book_comment.book_id = book.id
-    book_comment.save
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.save
   end
 
   def destroy
-    book_comment = BookComment.find_by(book_id: params[:book_id])
+    @book = Book.find(params[:book_id])
+    book_comment = @book.book_comments.find(params[:id])
     book_comment.destroy
   end
 
