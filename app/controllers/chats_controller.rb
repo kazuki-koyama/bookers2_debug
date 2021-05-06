17ed2a85d77a641381fb50a23ae208692cc7de3b
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-05-06 07:01:52 +0000

    [update] チャットviewを左右に振り分け

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -15,13 +15,14 @@ class ChatsController < ApplicationController
     @chats = @room.chats
     @chat = Chat.new(room_id: @room.id)
   end
+
   def create
     @chat = current_user.chats.new(chat_params)
     @chat.save
-    redirect_to request.referer
   end
 
   private
+
   def chat_params
     params.require(:chat).permit(:message, :room_id)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,9 @@ class User < ApplicationRecord
 
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  has_many :chats, dependent: :destroy
-  has_many :user_rooms, dependent: :destroy
+  has_many :chats
+  has_many :user_rooms
+  has_many :rooms, through: :user_rooms
 
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
