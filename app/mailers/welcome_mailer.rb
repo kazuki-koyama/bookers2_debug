6cc08bb1424970418b65421d957ebf664d99d64e
kazuki-koyama <kazuki.koyama92@gmail.com>
2021-05-06 01:16:51 +0000

    [add] deviseコントローラのカスタマイズファイルを生成

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -5,9 +5,9 @@ class WelcomeMailer < ApplicationMailer
   #
   #   en.welcome_mailer.send_when_signup.subject
   #
-  def send_when_signup(email, name)
-      @name = name
-      mail to: email, subject: 'Registration Complete! Thanks for Joining!'
+  def send_when_signup(user)
+      @user = user
+      mail(:subject => "Registration Complete! Thanks for Joining!", to: user.email)
   end
 
 end
